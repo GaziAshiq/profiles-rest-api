760a04662d6ae28a1efc4eabda85395929d73129
Gazi <GaziAshiq.dev@gmail.com>
2019-09-26 12:32:45 +0600

    Add A API get request

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 
 # Create your views here.
+
+class HelloApiView(APIView):
+    """Test API View"""
+
+    def get(self, request, format=None):
+        """Returns a list of APIView fratures"""
+        an_apiview = [
+            'Hello world',
+            'Hello Earth',
+            'Hello ocean',
+            'Hello sky',
+        ]
+        return Response({'message': 'Hey!', 'an_apiview': an_apiview})
